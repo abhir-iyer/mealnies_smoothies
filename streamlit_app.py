59ae9631e20d30d5d1866e62b3dcaf967a765dba
Abhir Iyer <98158477+abhir-iyer@users.noreply.github.com>
2026-06-16 21:00:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 # Streamlit app for Custom Smoothie Order Form - Badge 5 DABW
 # Co-authored with CoCo
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
